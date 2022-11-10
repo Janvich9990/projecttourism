01b7beb79f52ad6a8e71d195de593f13343c5218
Janvich9990 <107947662+Janvich9990@users.noreply.github.com>
2022-11-10 19:08:58 +0530

    Add files via upload

---
+++ navbar.js
@@ -0,0 +1,20 @@
+let appHeader=`
+// <!-- Navbar -->
+// <nav class="navbar background">
+//   <ul class="navlist">
+//       <div class="logo"><img src="logo.png"  alt="logo"></div>
+//       <li><a href="#home">Home</a></li>
+//       <li><a href="regformig.html">Login</a></li>
+//       <li><a href="TourPackages">Tour-Packages</a></li>
+//       <li><a href="#HotelBooking">Hotel-Booking</a></li>
+//       <li><a href="feedback2.html">Feedback</a></li>
+//       <li><a href="#Tour Guide">Contact us</a></li>
+//   </ul>
+//   <div class="rightnav">
+//       <input type="text"  Id="Search" name="Search">
+//       <button class="btn btn-sm">Search</button>
+//   </div>
+
+
+// </nav>
+// `;
